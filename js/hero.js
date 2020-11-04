8a82b089c0494fa80d381316ad488be3ddd01cf8
unknown <mounikareddygonae15@gmail.com>
2020-11-04 12:18:44 +0530

    add favourite page

--- js/hero.js
+++ js/hero.js
@@ -4,35 +4,30 @@
 const queryString = window.location.search;
 // to only extract specific URL Parameter value
 const urlParams = new URLSearchParams(queryString);
-
+// extracting name from url
 const name = urlParams.get('name')
-console.log(name);
+// console.log(name);
 
 const details = document.getElementById('details');
 
+// to create element
 function createNode(ele) {
         return document.createElement(ele);
 }
 
+// to append element to parent
 function append(parent, el) {
         return parent.appendChild(el);
 }
 
+// fetching the details of the super hero
 const searchHero = async name => {
-    console.log(name);
+ //     console.log(name);
     const res = await fetch(`https://www.superheroapi.com/api.php/3071693936250675/search/${name}`);
     const heros = await res.json();
     const results = heros.results[0];
-    console.log(results);
+ //     console.log(results);
     
-    // displayData(results);
-    // Object.entries(results).forEach(item =>
-    //     console.log(`${[0].biography} ${[0]}s each cost
-    //     ${[0].full-name}`)
-    //  );
-
-//     console.log(entries[2][0], ":" , entries[2][2]);
-        // results.map
         let nam = createNode('h1'),
         img  = createNode('img'),
         bio = createNode('h3'),
@@ -42,52 +37,30 @@ const searchHero = async name => {
         img.src = results.image.url;
         nam.innerHTML = results.name;
 
+ // bio details
         const entries1 = results.biography;
-        // console.log(entries1);
         for(const [key, value] of Object.entries(entries1)) {
-                console.log(`${key}  :  ${value}`);
+                // console.log(`${key}  :  ${value}`);
                 bio.innerHTML += (`${key} &nbsp;&nbsp; : &nbsp;&nbsp;&nbsp; ${value}<br><br>`);
         }
+ // powerstats
         const entries2 = results.powerstats;
         head.innerHTML =  `Powerstats:`
-        // console.log(entries2);
         for(const [key, value] of Object.entries(entries2)) {
-                console.log(`${key} : ${value}`);
-                
+                // console.log(`${key} : ${value}`);
                 pow.innerHTML += (`${key} &nbsp;&nbsp; : &nbsp;&nbsp;&nbsp; ${value}<br><br>`);
                 
                 
         }
-
+ //appending the data 
         append(details, nam);
         append(details, img);
         append(details, bio);
-        // append(details, head);
         append(details, pow);
-        
+  
+       
 };
 
 
 
-
-
-
-// const displayData = (matches) => {
-
-//     if(matches.length > 0) {
-//         // map returns an array from an array
-//         const html = matches.map(match => `
-         
-//        <div>
-//             <h4><a href="hero.html?name=${match.name}" > ${match.name} </a></h4>
-         
-//         <button style="color:blue; height:25px; margin-left:100px; "
-//          onclick = "favouriteList(${match.id})" > Add to Favorites </button>
-//          </div>
-//         `
-//         ).join('');
-        
-//         suggestion.innerHTML = html;
-//     }
-// };
 searchHero(name);
--- js/index.js
+++ js/index.js
@@ -4,17 +4,19 @@
 const search  = document.getElementById('search-bar');
 const suggestion = document.getElementById('suggestions');
 
-
+ // fetch api for searching
 const searchHero = async searchName => {
  const res = await fetch(`https://www.superheroapi.com/api.php/3071693936250675/search/${searchName}`);
  const heros = await res.json();
  const results = heros.results;
-//   console.log(results);
+ //   console.log(results);
+ // to get suggestions according to the input
  let matches = results.filter(hero => {
-     const regex = new RegExp(`^${searchName}`, 'gi');
-     return hero.name.match(regex);
+    //  regular expression is a way to search 
+    const regex = new RegExp(`^${searchName}`, 'gi');
+    return hero.name.match(regex);
  });
- console.log(matches);
+ //  console.log(matches);
 
 // if(searchName.length === 0 ) {
 //     matches = [];
@@ -22,35 +24,38 @@ const searchHero = async searchName => {
 // }
 
 displayData(matches);
-};
+}; 
 
-var favours = new Array();
-const favouriteList = (names) => {
-    // console.log(names);
-    favours.push(names);
-     localStorage.setItem("favours", JSON.stringify(favours));
-    // var storage = JSON.stringify(favours);
-    
-    // var storage = JSON.parse(localStorage['favours']);
-    // console.log(favours);
-    // console.log(storage);
+// local storage
+let names = localStorage.getItem('names')
+  ? JSON.parse(localStorage.getItem('names'))
+  : []
+//favourites  
+const favouriteList = fav => {
+    // console.log(fav);
+    names.push(fav);
+    localStorage.setItem("names", JSON.stringify(names));
     window.alert("Added to Favorites");
     
     
-    
 }
+//  console.log(names);
+
+// to display the results
 const displayData = (matches) => {
+    // if it's not empty
     if(matches.length > 0) {
         // map returns an array from an array
         const html = matches.map(match => `
          
        <div>
-            <h4><a href="hero.html?name=${match.name}" > ${match.name} </a></h4>
-         
-        <button style="color:blue; height:25px; margin-left:100px; "
-         onclick = "favouriteList(${match.id})" > Add to Favorites </button>
-         </div>
+            <h4> <a href="hero.html?name=${match.name}" > ${match.name} </a></h4>
+            
+            <button onclick = "favouriteList('${match.name}')" > Add to Favorites </button>
+
+        </div>
         `
+        // to convert it to string
         ).join('');
 
         suggestion.innerHTML = html;
